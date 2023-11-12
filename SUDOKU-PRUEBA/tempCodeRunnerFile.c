0f8ea8f994c0a930d8070076ad861f34019ebbba
Lautaro Tummino <lautarotummino@gmail.com>
2023-11-12 17:19:44 -0300

    Se elimina un archivo tempcoderunner

--- SUDOKU-PRUEBA/tempCodeRunnerFile.c
+++
@@ -1,5 +0,0 @@
-mna <= 9 && numero >= 1 && numero <= 9)
-        {
-            if (movimiento_valido(numero, tablero, fila - 1, columna - 1))
-            {
-                // Actualizar el tablero
